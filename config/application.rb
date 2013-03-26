6665cd5f799e627aa5043d69f344afb8ed82f5c2
dtakahas <dtcello@gmail.com>
2013-03-26 10:29:38 -0700

    Added basic email ticket notifications

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Ticketee
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For comment observer
+    config.active_record.observers = :comment_observer
   end
 end
